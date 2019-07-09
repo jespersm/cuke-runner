bf279571eb0a0fb1f04ee5fc389faf3254118b98
Jesper Steen Møller <jesper@selskabet.org>
2019-07-10 01:30:44 +0200

    Provide access to first row of DataTable, and to all rows. Crude, but works.

--- core/glue/src/step/argument/data_table.rs
+++ core/glue/src/step/argument/data_table.rs
@@ -1,4 +1,5 @@
 use gherkin::cuke;
+use step::argument::FromStepArgumentError;
 
 /// The lifetime parameter `'s` refers to the lifetime of the step.
 /// It cannot escape the step function.
@@ -19,6 +20,22 @@ impl<'s> DataTable<'s> {
     pub fn to_vec<T: FromDataTableRow<'s>>(&'s self) -> super::FromStepArgumentResult<Vec<T>> {
         self.cuke_table.rows.iter()
             .skip(1)
+            .map(|row| T::from_data_table_row(&row.cells))
+            .collect()
+    }
+
+    pub fn headers<T: FromDataTableRow<'s>>(&self) -> super::FromStepArgumentResult<T> {
+        if self.cuke_table.rows.len() >= 1 {
+            T::from_data_table_row(&self.cuke_table.rows[0].cells)
+        } else {
+            Err(FromStepArgumentError{message: "No rows".to_string()})
+        }
+    }
+
+    pub fn all_to_vec<T: FromDataTableRow<'s>>(&'s self) -> super::FromStepArgumentResult<Vec<T>> {
+        self.cuke_table
+            .rows
+            .iter()
             .map(|row| T::from_data_table_row(&row.cells))
             .collect()
     }
